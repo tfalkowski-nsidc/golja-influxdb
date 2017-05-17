8cc9582f3c41703c8e62a09833b7df3e8e649896
Thomas Falkowski <thomas.falkowski@nsidc.org>
2017-05-17 10:43:53 -0600

    Update service.pp
    Adding support for systemd

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,15 +6,27 @@ class influxdb::service(
 ){
 
   if $manage_service {
-
-    service { 'influxdb':
-      ensure     => $service_ensure,
-      enable     => $service_enabled,
-      hasrestart => true,
-      hasstatus  => true,
-      require    => Package['influxdb'],
+    if ($::osfamily == 'RedHat' and $::operatingsystemmajrelease == '7') or ($::operatingsystem == 'Ubuntu' and $::operatingsystemmajrelease >= '15.10') {
+      exec { 'refresh_systemd':
+        command     => 'systemctl daemon-reload',
+        refreshonly => true,
+      }
+      service { 'influxdb':
+        provider   =>  systemd,
+        ensure     => $service_ensure,
+        enable     => $service_enabled,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Package['influxdb'],
+      }
+    }else {
+      service { 'influxdb':
+        ensure     => $service_ensure,
+        enable     => $service_enabled,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Package['influxdb'],
+      }
     }
-
   }
-
 }
