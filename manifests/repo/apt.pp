86aa8296893a1894c0b1a8972c2b98ef1f2f0b6b
Thomas Falkowski <thomas.falkowski@nsidc.org>
2017-05-17 11:14:31 -0600

    Update apt.pp
    forcing apt-get update in order to override local ubuntu repo

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -23,6 +23,11 @@ class influxdb::repo::apt(
     repos    => 'stable',
     key      => $key,
     include  => $include,
+  }~>
+  
+  exec { 'apt-get-update':
+    command     => '/usr/bin/apt-get update',
+    refreshonly => true,
   }
 
 }
